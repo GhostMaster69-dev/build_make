5a2a13544ec10321eb79abc23fcf611f9af788a1
jhonboy121 <alfredmathew05@gmail.com>
2021-02-20 20:57:05 +0000

    releasetools: open ofile in binary mode
    * fixes build error when making recovery-from-boot.p

--- tools/releasetools/add_img_to_target_files.py
+++ tools/releasetools/add_img_to_target_files.py
@@ -204,7 +204,7 @@ def AddVendor(output_zip, recovery_img=None, boot_img=None):
     return img.name
 
   def output_sink(fn, data):
-    ofile = open(os.path.join(OPTIONS.input_tmp, "VENDOR", fn), "w")
+    ofile = open(os.path.join(OPTIONS.input_tmp, "VENDOR", fn), "wb")
     ofile.write(data)
     ofile.close()
 
